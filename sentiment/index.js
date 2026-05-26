6800af29cd10a45b1712527661f1e66dbbf418e4
Sizarh <123359300+Sizarh@users.noreply.github.com>
2026-05-26 20:50:03 +0200

    Update sentiment/index.js with natural import and example

---
+++ sentiment/index.js
@@ -0,0 +1,6 @@
+// Import the natural npm package
+const natural = require("natural");
+
+// Example usage: tokenize a sentence
+const tokenizer = new natural.WordTokenizer();
+console.log(tokenizer.tokenize("I love learning about sentiment analysis!"));
